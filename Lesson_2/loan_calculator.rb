9c33b96a6defa67eb97e88de66e70deca9bf041f
Syed <syed@Syed.localdomain>
2023-03-09 20:39:04 +0000

    forgot about rubocop, also found issues when apr == 0

--- Lesson_2/loan_calculator.rb
+++ Lesson_2/loan_calculator.rb
@@ -14,12 +14,12 @@ def interest_calculator(loan, interest, duration)
   loan * (interest / (1 - (1 + interest)**(-(duration))))
 end
 
-def user_input_loan_amount()
-  while true
+def user_input_loan_amount
+  loop do
     prompt "Please state the value of the loan in plain digits e.g."
     prompt "'£10000' = 10000"
     loan_value = gets.chomp
-    if valid_money?(loan_value) 
+    if valid_money?(loan_value)
       prompt "#{loan_value}.00 in your local currency."
       return loan_value.to_f
     else
@@ -28,13 +28,13 @@ def user_input_loan_amount()
   end
 end
 
-def user_input_years()
-  while true
+def user_input_years
+  loop do
     prompt "Please state the loan duration in years rounded down"
     prompt "e.g '12 years and 11 months' = 12"
     prompt "Years remaining:"
     years = gets.chomp
-    if valid_number?(years) 
+    if valid_number?(years)
       prompt "#{years.to_i} years."
       return years.to_i
     else
@@ -43,13 +43,13 @@ def user_input_years()
   end
 end
 
-def user_input_months()
-  while true
+def user_input_months
+  loop do
     prompt "Please state any additional months remaining on your loan duration"
     prompt "e.g. '12 years and 11 months' = 11"
     prompt "Months remaining:"
     months = gets.chomp
-    if valid_number?(months) 
+    if valid_number?(months)
       prompt "#{months.to_i} months."
       return months.to_i
     else
@@ -58,8 +58,8 @@ def user_input_months()
   end
 end
 
-def user_input_apr()
-  while true
+def user_input_apr
+  loop do
     prompt "What is your annual percentage rate? e.g '5%' = 5"
     apr = gets.chomp
     if valid_number?(apr)
@@ -71,14 +71,16 @@ def user_input_apr()
   end
 end
 
-def display_final_results(monthly_payments_var, loan_duration_var, loan_amount_var)
+def display_final_results(monthly_payments_var, loan_duration_var,
+                          loan_amount_var)
   prompt "Calculating..."
   prompt "Your monthly payment amount is #{monthly_payments_var.round(2)}."
-  prompt "Your total payment amount is #{(monthly_payments_var * loan_duration_var).round(2)}."
+  prompt "Your total payment amount is #{(monthly_payments_var *
+  loan_duration_var).round(2)}."
   prompt "The total interest on your loan is #{((monthly_payments_var.round(2) *
   loan_duration_var) - loan_amount_var).round(2).abs}."
 end
-  
+
 prompt "This is a loan calculator used to determine your monthly payments,"
 prompt "total payment amount, and total interest value."
 
@@ -91,11 +93,13 @@ loop do
     if total_years + total_months > 0
       break
     else
-      prompt "both years and months cannot be 0, please input a valid time frame"
+      prompt "both years and months cannot be 0,
+      please input a valid time frame"
     end
   end
 
-  prompt "Total loan duration is #{total_years} years and #{total_months} months."
+  prompt "Total loan duration is #{total_years} years and
+  #{total_months} months."
   loan_duration = (total_years * 12) + total_months
 
   yearly_interest = user_input_apr
@@ -105,10 +109,13 @@ loop do
   monthly_interest = (yearly_interest / 12.0) / 100 # divide by 100 to get the
   # appropriate decimal value for arithmetic
 
-  monthly_payments = interest_calculator(loan_amount, monthly_interest, loan_duration)
+  monthly_payments = interest_calculator(loan_amount, monthly_interest,
+                                         loan_duration)
 
-  display_final_results(monthly_payments, loan_duration, loan_amount)
-  
+  display_final_results(monthly_payments, loan_duration,
+                        loan_amount) # displays NaN if interest == 0; needs an
+  # if clause, or only allow apr > 0
+
   prompt "Thanks for using the calculator; would you like to make another"
   prompt "calculation? (y/n)"
   input = gets.chomp
